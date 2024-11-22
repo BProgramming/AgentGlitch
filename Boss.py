eca6cf4002827d15c9752e7421939efc6fdb2de1
BProgramming <brentjpollard@gmail.com>
2024-11-22 13:41:22 -0500

    Add files via upload

--- Boss.py
+++ Boss.py
@@ -19,8 +19,8 @@ class Boss(Enemy):
         self.is_animated_attack = True
         self.audio_trigger_frames.update({"WIND_UP": [0], "ATTACK_ANIM": [0], "WIND_DOWN": [0]})
 
-    async def queue_music(self):
-        if self.music_is_playing and math.dist((self.level.get_player().rect.x, self.level.get_player().rect.y), (self.rect.x, self.rect.y)) >= 2 * self.spot_range:
+    async def queue_music(self, force_stop=False):
+        if self.music_is_playing and (force_stop or math.dist((self.level.get_player().rect.x, self.level.get_player().rect.y), (self.rect.x, self.rect.y)) >= 2 * self.spot_range):
             if pygame.mixer.music.get_busy():
                 pygame.mixer.music.fadeout(1000)
             self.controller.queue_track_list()
@@ -31,14 +31,19 @@ class Boss(Enemy):
             self.controller.queue_track_list(music=self.music)
             self.music_is_playing = True
 
-    async def toggle_music(self):
-        await self.queue_music()
+    async def toggle_music(self, force_stop=False):
+        await self.queue_music(force_stop=force_stop)
         pygame.event.Event(pygame.USEREVENT)
+
+    def get_hit(self, obj, hit_cd=0, heal_delay=0):
+        super().get_hit(obj)
+        if self.hp <= 0:
+            asyncio.run(self.toggle_music(force_stop=True))
 
     def update_sprite(self, fps, delay=0):
         active_index = super().update_sprite(fps)
         if self.audios is not None and (self.state in [MovementState.WIND_UP, MovementState.ATTACK_ANIM, MovementState.WIND_DOWN]) and self.audio_trigger_frames.get(str(self.state)) is not None:
-            audio_folder = self.name.upper() + "_" + str(self.state)
+            audio_folder = self.name.upper().split(" ",1)[0] + "_" + str(self.state)
             if self.audios.get(audio_folder) is not None and active_index in self.audio_trigger_frames[str(self.state)]:
                 self.active_audio = self.audios[audio_folder][random.randrange(len(self.audios[audio_folder]))]
                 if self.active_audio_channel is not None:
