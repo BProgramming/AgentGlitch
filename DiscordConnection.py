ee1d63693acc443700327edc44313f2356154fc8
Brent Pollard <brentjpollard@gmail.com>
2025-11-10 21:04:59 -0500

    Bug fix

--- DiscordConnection.py
+++ DiscordConnection.py
@@ -8,7 +8,7 @@ class DiscordConnection:
     def __init__(self) -> None:
         try:
             self.presence: Presence | None = Presence(DiscordConnection.CLIENT_ID)
-        except PresenceError:
+        except Exception:
             self.presence = None
         self.start_time: int = int(time.time())
         self.activity: dict[str, str | dict[str, int]] = {"state": "", "details": "", "timestamps": {"start": self.start_time}}
@@ -19,7 +19,7 @@ class DiscordConnection:
             self.activity["state"] = state
             try:
                 self.presence.set(self.activity)
-            except PresenceError:
+            except Exception:
                 pass
 
     def close(self) -> None:
@@ -27,5 +27,5 @@ class DiscordConnection:
             try:
                 self.presence.clear()
                 self.presence.close()
-            except PresenceError:
+            except Exception:
                 pass
